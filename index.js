faa993349adab3afc0570dcd14d728067d65549b
Mar-Issah <masy370@gmail.com>
2022-03-25 18:28:43 -0400

    Add js file

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('hi');
+const userCardTemplate = document.querySelector('[data-user-template]');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((re) => res.json())
+  .then((data) => {});
